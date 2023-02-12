0c78631707fee5a44ae4c244d32431594494f878
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-12 22:46:53 +0500

    added new task

--- example test 5/Program.cs
+++ example test 5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 7;
+int b = 4;
+int c = 2;
+int d = 15;
+int e = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
