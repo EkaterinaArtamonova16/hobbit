0187f82787d8424c36fbb1b2d613bf76d090c9f3
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-12 22:13:18 +0500

    added info to file

--- example test 4/Program.cs
+++ example test 4/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 37;
+double numberB = 9;
+Console.WriteLine(numberA / numberB);
